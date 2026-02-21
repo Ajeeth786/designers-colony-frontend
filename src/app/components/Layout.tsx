968f12d6789f09a33697948f1348bff67554d8e1
Ajeeth Kumar <ajeeth.kumar@marlensoft.com>
2026-02-21 14:47:35 +0530

    Added Google Analytics

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,12 +1,28 @@
 import { Outlet } from "react-router";
 import { Header } from "./Header";
+import { useEffect } from "react";
 
 export function Layout() {
+
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX";
+    script1.async = true;
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-XXXXXXXXXX');
+    `;
+    document.head.appendChild(script2);
+    }, []);
+
   return (
     <>
       <Header />
-
-      {/* Global page offset for fixed header */}
       <main className="pt-[40px] sm:pt-[56px] bg-[#FAFAF9] min-h-screen">
         <Outlet />
       </main>
